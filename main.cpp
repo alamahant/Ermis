35dacfb0abe2ae0b24b92914689d92c63b1280de
Alamahant <alamahant@gmail.com>
2026-06-15 17:00:34 +0300

    preserve theme

--- main.cpp
+++ main.cpp
@@ -25,6 +25,24 @@ int main(int argc, char *argv[])
     //QDir().mkpath(Constants::musicPath);
 
     QApplication a(argc, argv);
+
+#ifndef FLATPAK_BUILD
+
+    a.setStyle(QStyleFactory::create("Fusion"));
+
+    QPalette lightPalette;
+    lightPalette.setColor(QPalette::Window, Qt::white);
+    lightPalette.setColor(QPalette::WindowText, Qt::black);
+    lightPalette.setColor(QPalette::Base, Qt::white);
+    lightPalette.setColor(QPalette::Text, Qt::black);
+    lightPalette.setColor(QPalette::Button, QColor(240, 240, 240));
+    lightPalette.setColor(QPalette::ButtonText, Qt::black);
+    lightPalette.setColor(QPalette::Highlight, QColor(0, 120, 215));
+    lightPalette.setColor(QPalette::HighlightedText, Qt::white);
+
+    a.setPalette(lightPalette);
+#endif
+
     a.setWindowIcon(QIcon(":/io.github.alamahant.Ermis.png"));
     MainWindow w;
     w.show();
